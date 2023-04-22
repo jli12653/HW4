c6341e6bf79d3aae2d87c5364afbf2a4609c9d98
jli12653 <124959187+jli12653@users.noreply.github.com>
2023-04-21 22:55:01 -0400

    1

--- int_ring.cpp
+++ int_ring.cpp
@@ -3,12 +3,12 @@
 
 int main(int argc, char *argv[]) {
   int rank, p;
-  int N = 1000000;
 
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &p);
-
+  
+  int N = 1000000;
   double tt = MPI_Wtime();
 
   for (int i = 0; i< N;i++){
