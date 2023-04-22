8c1e3a365194870f8a23a6c989a24be37953c6b8
jli12653 <124959187+jli12653@users.noreply.github.com>
2023-04-21 23:41:22 -0400

    1

--- int_ring.cpp
+++ int_ring.cpp
@@ -9,48 +9,70 @@ int main(int argc, char *argv[]) {
   MPI_Comm_size(MPI_COMM_WORLD, &p);
   
   int N = 1000000;
+  
   double tt = MPI_Wtime();
 
   for (int i = 0; i< N;i++){
 
   if (rank == 0) {
-    int message_out = 0;
-    int message_in;
+    // int message_out = 0;
+    // int message_in;
 
     MPI_Status status;
     //MPI_Request request_out, request_in;
 
-    MPI_Send(&message_out, 1, MPI_INT, 1, 999, MPI_COMM_WORLD);
+    // MPI_Send(&message_out, 1, MPI_INT, 1, 999, MPI_COMM_WORLD);
 
-    MPI_Recv(&message_in,  1, MPI_INT, p-1, 999, MPI_COMM_WORLD, &status);
+    // MPI_Recv(&message_in,  1, MPI_INT, p-1, 999, MPI_COMM_WORLD, &status);
+
+    int * array_out    = (int*) malloc(N);;
+    for (int j = 0; j < N; j++) array_out[j] = 12;
+
+    int * array_in;
+
+    MPI_Send(array_out, N, MPI_INT, 1, 999, MPI_COMM_WORLD);
+
+    MPI_Recv(array_in,  N, MPI_INT, p-1, 999, MPI_COMM_WORLD, &status);
 
     //printf("Rank %d in %d received %d\n", rank, p, message_in);
 
-    if (i == N-1) printf("Rank %d in %d received %d\n", rank, p, message_in);
+    //if (i == N-1) printf("Rank %d in %d received %d\n", rank, p, message_in);
 
   } else if (rank == p-1) {
-    int message_out = 0;
-    int message_in;
+    // int message_out = 0;
+    // int message_in;
+
+    int * array_out;
+    int * array_in = (int*) malloc(N);
 
     MPI_Status status;
 
-    MPI_Recv(&message_in, 1, MPI_INT, p-2, 999, MPI_COMM_WORLD, &status);
-    message_out = message_in + p-1;
+    // MPI_Recv(&message_in, 1, MPI_INT, p-2, 999, MPI_COMM_WORLD, &status);
+    // MPI_Send(&message_out, 1, MPI_INT, 0, 999, MPI_COMM_WORLD);
 
-    MPI_Send(&message_out, 1, MPI_INT, 0, 999, MPI_COMM_WORLD);
+    MPI_Recv(array_in,  N, MPI_INT, p-2, 999, MPI_COMM_WORLD, &status);
+    array_out = array_in;
+    MPI_Send(array_out, N, MPI_INT, 0, 999, MPI_COMM_WORLD);
 
     //printf("The message is %d\n", message_in);
   }
   else{
-  int message_out = 0;
-  int message_in;
+  // int message_out = 0;
+  // int message_in;
 
   MPI_Status status;
 
-  MPI_Recv(&message_in, 1, MPI_INT, rank-1, 999, MPI_COMM_WORLD, &status);
-  message_out = message_in + rank;
+  // MPI_Recv(&message_in, 1, MPI_INT, rank-1, 999, MPI_COMM_WORLD, &status);
+  // message_out = message_in + rank;
 
-  MPI_Send(&message_out, 1, MPI_INT, rank+1, 999, MPI_COMM_WORLD);
+  // MPI_Send(&message_out, 1, MPI_INT, rank+1, 999, MPI_COMM_WORLD);
+
+  int * array_out;
+  int * array_in = (int*) malloc(N);
+
+  MPI_Recv(array_in,  N, MPI_INT, rank-1, 999, MPI_COMM_WORLD, &status);
+  array_out = array_in;
+  MPI_Send(array_out, N, MPI_INT, rank+1, 999, MPI_COMM_WORLD);
   }
 
   }
