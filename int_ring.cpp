f997810e1fe22c32ac1902b7d57c3f06be5e4892
jli12653 <124959187+jli12653@users.noreply.github.com>
2023-04-23 00:15:16 -0400

    1

--- int_ring.cpp
+++ int_ring.cpp
@@ -9,8 +9,8 @@ int main(int argc, char *argv[]) {
   MPI_Comm_size(MPI_COMM_WORLD, &p);
   
   int N = 1000000;
-  int* array = (int*) malloc(N * sizeof(int)); 
-  for (int j = 0; j < N; j++) array[j] = 12;
+  //int* array = (int*) malloc(N * sizeof(int)); 
+  //for (int j = 0; j < N; j++) array[j] = 12;
   MPI_Barrier(MPI_COMM_WORLD);
   
   double tt = MPI_Wtime();
@@ -18,69 +18,57 @@ int main(int argc, char *argv[]) {
   for (int i = 0; i< N;i++){
 
   if (rank == 0) {
-    // int message_out = 0;
-    // int message_in;
+    int message_out = 0;
+    int message_in;
 
     MPI_Status status;
-    //MPI_Request request_out, request_in;
+    MPI_Request request_out, request_in;
 
-    // MPI_Send(&message_out, 1, MPI_INT, 1, 999, MPI_COMM_WORLD);
+    MPI_Send(&message_out, 1, MPI_INT, 1, i, MPI_COMM_WORLD);
 
-    // MPI_Recv(&message_in,  1, MPI_INT, p-1, 999, MPI_COMM_WORLD, &status);
+    MPI_Recv(&message_in,  1, MPI_INT, p-1, i, MPI_COMM_WORLD, &status);
 
-    //int * array_out    = (int*) malloc(N);
-    
 
-    //int * array_in = (int*) malloc(N);
+    // MPI_Send(array, N, MPI_INT, 1, i, MPI_COMM_WORLD);
 
-    MPI_Send(array, N, MPI_INT, 1, i, MPI_COMM_WORLD);
-
-    MPI_Recv(array,  N, MPI_INT, p-1, i, MPI_COMM_WORLD, &status);
+    // MPI_Recv(array,  N, MPI_INT, p-1, i, MPI_COMM_WORLD, &status);
 
     //printf("Rank %d in %d received %d\n", rank, p, message_in);
 
-    //if (i == N-1) printf("Rank %d in %d received %d\n", rank, p, message_in);
+    if (i == N-1) printf("Rank %d in %d received %d\n", rank, p, message_in);
     
     //free(array_out);
 
   } else if (rank == p-1) {
-    // int message_out = 0;
-    // int message_in;
+    int message_out = 0;
+    int message_in;
 
-    //int * array_out = (int*) malloc(N);
-    //int * array_in = (int*) malloc(N);
 
     MPI_Status status;
 
-    // MPI_Recv(&message_in, 1, MPI_INT, p-2, 999, MPI_COMM_WORLD, &status);
-    // MPI_Send(&message_out, 1, MPI_INT, 0, 999, MPI_COMM_WORLD);
+    MPI_Recv(&message_in, 1, MPI_INT, p-2, 999, MPI_COMM_WORLD, &status);
+    message_out = message_in + rank;
+    MPI_Send(&message_out, 1, MPI_INT, 0, 999, MPI_COMM_WORLD);
 
-    MPI_Recv(array,  N, MPI_INT, p-2, i, MPI_COMM_WORLD, &status);
-    //array_out = array_in;
-    MPI_Send(array, N, MPI_INT, 0, i, MPI_COMM_WORLD);
+    // MPI_Recv(array,  N, MPI_INT, p-2, i, MPI_COMM_WORLD, &status);
+    // MPI_Send(array, N, MPI_INT, 0, i, MPI_COMM_WORLD);
     
-    //printf("The message is %d\n", message_in);
 
     
-    //free(array_out);
   }
   else{
-  // int message_out = 0;
-  // int message_in;
+  int message_out = 0;
+  int message_in;
 
   MPI_Status status;
 
-  // MPI_Recv(&message_in, 1, MPI_INT, rank-1, 999, MPI_COMM_WORLD, &status);
-  // message_out = message_in + rank;
+  MPI_Recv(&message_in, 1, MPI_INT, rank-1, 999, MPI_COMM_WORLD, &status);
+  message_out = message_in + rank;
+  MPI_Send(&message_out, 1, MPI_INT, rank+1, 999, MPI_COMM_WORLD);
 
-  // MPI_Send(&message_out, 1, MPI_INT, rank+1, 999, MPI_COMM_WORLD);
-
-  //int * array_out = (int*) malloc(N);
-  //int * array_in = (int*) malloc(N);
-
-  MPI_Recv(array,  N, MPI_INT, rank-1, i, MPI_COMM_WORLD, &status);
-  //array_out = array_in;
-  MPI_Send(array, N, MPI_INT, rank+1, i, MPI_COMM_WORLD);
+ 
+  // MPI_Recv(array,  N, MPI_INT, rank-1, i, MPI_COMM_WORLD, &status);
+  // MPI_Send(array, N, MPI_INT, rank+1, i, MPI_COMM_WORLD);
 
   }
 
@@ -93,7 +81,7 @@ int main(int argc, char *argv[]) {
     printf("Time elapsed is %f seconds.\n", elapsed);
   }
 
-  free(array);
+  //free(array);
 
   MPI_Finalize();
 
